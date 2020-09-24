692ee33983a36acc383c3a2a469040e6ce5e8fa0
Eric Lewiston <eric.lewiston.el@gmail.com>
2020-09-23 22:13:29 -0500

    added default docs, added code to populate table

---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+// helps correct strrange character sequences in text
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+
+// build table for each data
+function buildTable(filteredData){
+    // select the body of the table
+    var tbody = d3.select("tbody");
+    
+    // loop through each object
+    filteredData.forEach((sighting) => {
+        // create a new row
+        var row = tbody.append("tr");
+
+        // loop through each object entry
+        Object.entries(sighting).forEach(([key, value]) => {
+
+            // create new row, clean up text
+            var cell = row.append("td");
+            cell.text(decodeHtml(value));
+        });
+    });
+}
+
+// from data.js
+var tableData = data;
+
+// default table
+buildTable(tableData)
+
+
+// create event handler for button click/filter
+
